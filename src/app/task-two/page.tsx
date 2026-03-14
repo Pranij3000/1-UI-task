42b1a3f3af0ec617022a9eb73bee36a5c44f75ea
Pranij Manandhar <pranijmanandhar@gmail.com>
2026-03-14 13:54:07 +0545

    animation setup completed

--- src/app/task-two/page.tsx
+++ src/app/task-two/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Class } from "../../data/Classes";
 import ClassCard from "@/src/components/cards/ClassCard";
 
 export default function page() {
   const [active, setActive] = useState<number>(0);
+  const [prevActive, setPrevActive] = useState<number>(0);
+
+  const handleActiveChange = (index: number) => {
+    setPrevActive(active);
+    setActive(index);
+  };
   return (
     <section className="task-two overflow-hidden py-15">
       <div className="container max-w-314 px-5 mx-auto">
@@ -16,14 +22,16 @@ export default function page() {
             Dive Into <span className="text-primary-green">What’s Hot Right Now! 🔥</span>
           </h1>
         </div>
-        <div className="cards-wrapper flex gap-8">
+        <div className="cards-wrapper flex flex-col lg:flex-row gap-8">
           {Class.map((item, index) => (
             <ClassCard
               key={index}
               index={index}
               info={item}
               isActive={index === active}
-              setActive={setActive}
+              setActive={handleActiveChange}
+              prevIndex={prevActive}
+              activeIndex={active}
             />
           ))}
         </div>
--- src/components/cards/ClassCard.tsx
+++ src/components/cards/ClassCard.tsx
@@ -1,5 +1,30 @@
+"use client";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useRef, useState } from "react";
+
+const courseLogo = [
+  {
+    img: "/images/course-logo-01.png",
+    aspect: "square",
+    rotate: "-16.67deg",
+  },
+  {
+    img: "/images/course-logo-02.png",
+    aspect: "square",
+    rotate: "7.22deg",
+  },
+  {
+    img: "/images/course-logo-03.png",
+    aspect: "55/75",
+    rotate: "-9deg",
+  },
+  {
+    img: "/images/course-logo-04.png",
+    aspect: "square",
+    rotate: "12.61deg",
+  },
+];
 
 interface ClassCard {
   number: string;
@@ -7,31 +32,189 @@ interface ClassCard {
   description: string;
 }
 
-export default function ClassCard({ info, isActive, setActive, index }: { info: ClassCard; isActive: boolean; setActive: React.Dispatch<React.SetStateAction<number>>; index: number }) {
-  useGSAP(() => {
-    const tl = gsap.timeline({
-      defaults: {
-        duration: 1,
-      },
-    });
-  });
+export default function ClassCard({ info, isActive, setActive, index, prevIndex, activeIndex }: { info: ClassCard; isActive: boolean; setActive: (index: number) => void; index: number; prevIndex: number; activeIndex: number }) {
+  const card = useRef(null);
+
+  useGSAP(
+    () => {
+      if (isActive) {
+        const tl = gsap.timeline();
+        tl.set(".view-button-wrapper", {
+          translateX: "0",
+        });
+
+        if (prevIndex < index) {
+          tl.set(".course-container", {
+            translateX: "-100%",
+          });
+        } else if (prevIndex > index) {
+          tl.set(".course-container", {
+            translateX: "100%",
+          });
+        }
+
+        // if (prevIndex < index) {
+        // } else if (prevIndex > index) {
+        //   tl.set(".course-container", {
+        //     translateX: "-100%",
+        //   });
+        // }
+
+        tl.to(card.current, {
+          flexGrow: 0.8,
+          duration: 0.3,
+        });
+        tl.to(card.current, {
+          flexGrow: 2.1,
+          duration: 0.5,
+        });
+
+        tl.to(
+          ".background-pink",
+          {
+            width: "0%",
+            delay: 0.3,
+            duration: 0.5,
+          },
+          "0",
+        );
+
+        tl.to(
+          ".view-button-wrapper",
+          {
+            duration: 1,
+            opacity: 1,
+          },
+          "0",
+        );
+
+        tl.to(
+          ".course-container",
+          {
+            translateX: 0,
+            duration: 0.5,
+            opacity: 1,
+          },
+          "0",
+        );
+      } else {
+        const tl = gsap.timeline();
+
+        tl.to(card.current, {
+          flexGrow: 2.4,
+          duration: 0.3,
+        });
+        tl.to(card.current, {
+          flexGrow: 1,
+          duration: 0.5,
+        });
+
+        tl.to(
+          ".background-pink",
+          {
+            width: "500%",
+            delay: 0.3,
+            duration: 0.5,
+          },
+          "0",
+        );
+
+        tl.to(
+          ".view-button-wrapper",
+          {
+            duration: 0.3,
+            translateX: "-50px",
+          },
+          "0",
+        );
+        tl.to(
+          ".view-button-wrapper",
+          {
+            duration: 0.7,
+            translateX: "200px",
+            opacity: 0,
+          },
+          ">",
+        );
+
+        if (activeIndex > index) {
+          tl.to(
+            ".course-container",
+            {
+              translateX: "100%",
+              duration: 0.5,
+              opacity: 0,
+            },
+            "0",
+          );
+        } else {
+          tl.to(
+            ".course-container",
+            {
+              translateX: "-100%",
+              duration: 0.5,
+              opacity: 0,
+            },
+            "0",
+          );
+        }
+      }
+    },
+    { dependencies: [isActive], scope: card },
+  );
   return (
     <>
       <div
+        ref={card}
         onClick={() => setActive(index)}
-        className={`class-card py-10 px-9 rounded-[32px] overflow-hidden ${isActive ? "flex-2 bg-dark-pink" : "flex-1 bg-light-pink cursor-pointer"}`}
+        className={`class-card flex-[1_1_0%]  relative rounded-[32px] overflow-hidden bg-dark-pink ${isActive ? "" : "cursor-pointer"}`}
       >
-        <div className="course-detail max-h-60.5"></div>
-        <div className={`relative course-title w-fit mx-auto  flex items-center gap-6 `}>
-          {/* <div className={`number relative `}>
+        <div className="background-pink absolute left-0 top-full aspect-square bg-light-pink rounded-full translate-x-[-50%] translate-y-[-50%]" />
+        <div className="content-wrapper px-9 py-10">
+          <div className="course-detail h-60.5">
+            <div className="active-detail pb-16 flex flex-col gap-5 h-full justify-between">
+              <div className="view-button-wrapper w-fit ml-auto">
+                <button className="flex items-center gap-2 cursor-pointer">
+                  <span className="font-outfit font-semibold text-[18px] text-white">View all Courses</span>
+                  <div className="icon-wrapper aspect-square w-5">
+                    <img
+                      src="/images/arrow-right-white.png"
+                      alt=""
+                      className="w-full h-auto"
+                    />
+                  </div>
+                </button>
+              </div>
+              <div className={`course-container`}>
+                <div className="course-icon-wrapper max-w-115 mx-auto flex items-center justify-between gap-5">
+                  {courseLogo.map((item, index) => (
+                    <div
+                      key={index}
+                      style={{ aspectRatio: item.aspect, transform: `rotate(${item.rotate})` }}
+                      className="img-wrapper h-19"
+                    >
+                      <img
+                        src={item.img}
+                        alt=""
+                        className="h-full w-auto"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className={`relative course-title w-fit mx-auto  flex items-center gap-6 `}>
+          <div className={`number relative `}>
             <strong className={`font-nohemi text-[150px] leading-[120%]  transition-all ease-in-out duration-500 ${isActive ? "text-[#F9EBEC]" : "text-dark-pink"}`}>{info.number}</strong>
             <span className={`absolute -top-6 -right-6 font-nohemi text-[64px]  transition-all ease-in-out duration-500 ${isActive ? "text-[#F9EBEC]" : "text-dark-pink"}`}>+</span>
           </div>
           <div className={`detail-wrapper `}>
             <h2 className={`font-outfit  transition-all ease-in-out duration-500 text-[32px] font-bold ${isActive ? "text-[#F9EBEC]" : "text-dark-pink"}`}>{info.title}</h2>
             <p className={`font-outfit  transition-all ease-in-out duration-500 text-[18px] ${isActive ? "text-[#F9EBEC]" : "text-dark-pink"}`}>{info.description}</p>
-          </div> */}
-        </div>
+          </div>
+        </div> */}
       </div>
     </>
   );
